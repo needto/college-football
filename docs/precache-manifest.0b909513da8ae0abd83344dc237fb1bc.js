ff8088c8ea616db14db69b004aed9d49e3f3d5b7
needto <needto@gmail.com>
2019-10-14 17:56:13 +0200

    Build app into ./docs for Github Pages deployment

---
+++ docs/precache-manifest.0b909513da8ae0abd83344dc237fb1bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf1a2a608bca4e783ab937ad0c96213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27abffb73d16ba0b9831",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6be9406f2d70fb150ff4",
+    "url": "/static/css/main.e6e4b823.chunk.css"
+  },
+  {
+    "revision": "27abffb73d16ba0b9831",
+    "url": "/static/js/2.561510c1.chunk.js"
+  },
+  {
+    "revision": "6be9406f2d70fb150ff4",
+    "url": "/static/js/main.17364f57.chunk.js"
+  },
+  {
+    "revision": "07f33d6160bd949e28f0",
+    "url": "/static/js/runtime-main.b44f18ce.js"
+  }
+]);
